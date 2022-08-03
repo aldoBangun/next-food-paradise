4ccdd804c82b96899ea54c74c2eb8ba5d797e82d
aldoBangun <punk.aldo68@gmail.com>
2022-08-03 09:26:13 +0800

    Update styling

--- components/layout/NavLink.js
+++ components/layout/NavLink.js
@@ -7,11 +7,13 @@ const NavLink = ({ link, label, children }) => {
   const router = useRouter()
   const { asPath } = router
   const isActive = asPath === link
+  // const activeLink = ['text-primary bg-verylightblue bg-opacity-10', 'text-white bg-primary']
 
   return (
     <Nav.Item className={style.navItem}>
       <Link href={link} passHref>
-        <a className={`${isActive ? 'text-primary bg-verylightblue bg-opacity-10' : ''} d-flex align-items-center gap-2 p-2 rounded-2 text-decoration-none`}>
+        {/* <a className={`${isActive ? activeLink[1] : ''} d-flex align-items-center gap-2 p-2 rounded-3 text-decoration-none`}> */}
+        <a className={`${style.link} ${isActive ? style.isActive : ''}`}>
           {children}
           {isActive && (<span>{label}</span>)}
         </a>
