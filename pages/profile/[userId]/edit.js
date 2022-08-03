69ccf4ea934e8da5ceb4e12a515e7bd5ebe40b6f
aldoBangun <punk.aldo68@gmail.com>
2022-08-04 05:26:42 +0800

    Update layout

--- pages/profile/[userId]/edit.js
+++ pages/profile/[userId]/edit.js
@@ -1,10 +1,21 @@
 import Layout from '@/components/layout/Layout'
 import { Container } from 'react-bootstrap'
+import EditOptions from '@/components/profile/EditOptions'
+import EditActions from '@/components/profile/EditActions'
+import style from '@/styles/Profile.module.css'
 
 const Edit = () => {
   return (
     <Container>
-      <h1>Edit Profile</h1>
+      <div className={style.editProfileContainer}>
+        <EditOptions />
+
+        <div className={style.editProfileActions}>
+          <Container>
+            <EditActions />
+          </Container>
+        </div>
+      </div>
     </Container>
   )
 }
