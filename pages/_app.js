b368a6681ba65d730e71d87cfcdedca87e91de2a
aldoBangun <punk.aldo68@gmail.com>
2022-08-09 07:32:25 +0800

    Add redux/persist and axios interceptor

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,44 @@
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '../app/store'
+import { Provider } from 'react-redux'
 import '../styles/globals.css'
 import '../styles/scss/globals.scss'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL
+
+
+axios.interceptors.request.use((config) => {
+  if(typeof window !== 'undefined') {
+    const storage = localStorage.getItem('persist:root')
+  
+    if(storage) {
+      const storageJSON = JSON.parse(storage)
+      const token = storageJSON?.auth
+
+      if(config?.category !== "noAuth" && token) {
+        config.headers = {
+          Authorization : `Bearer ${token}`
+        }
+      }
+    }
+  }
+
+  return config
+}, (error) => {
+  Promise.reject(error)
+})
 
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || (page => page)  
-  return getLayout(<Component {...pageProps} />)
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      {getLayout(<Component {...pageProps} />)}
+    </PersistGate>
+  </Provider>
+  )
 }
 
 export default MyApp
