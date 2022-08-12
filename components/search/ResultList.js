28858a003f86cd79b5d1e764faf31db6e9140a8f
aldoBangun <punk.aldo68@gmail.com>
2022-08-12 14:32:48 +0800

    Add result not found response

--- components/search/ResultList.js
+++ components/search/ResultList.js
@@ -1,16 +1,20 @@
 import RecipePopularItem from '@/components/home/RecipePopularItem'
+import { useRouter } from 'next/router'
 
-const ResultList = () => {
+const ResultList = ({ result = [] }) => {
+  const router = useRouter()
+  const keyword = router?.query?.keyword
+
   return (
     <>
       {result.length ? (
         <ul>
-          {result.map(item => (
+          {result.map((item) => (
             <RecipePopularItem key={item.recipe_id} {...item} />
           ))}
         </ul>
       ) : (
-        <h3> `&quot`{search}`&quot` not found</h3>
+        <h4 className="text-gray"> {keyword ? <>&quot;{keyword}&quot;</> : 'Result'} not found</h4>
       )}
     </>
   )
